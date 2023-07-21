c8518686f34e185eaee890bb2926712fdddce669
tumelorabale <133318673+tumelorabale@users.noreply.github.com>
2023-07-20 20:11:43 -0500

    yml added6

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
 	const [searchValue, setSearchValue] = useState('');
 
 	const getMovieRequest = async (searchValue) => {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+		const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=dfc9f431`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
