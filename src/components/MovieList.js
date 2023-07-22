dc6e8b0c1a59dae48a24cbddf579775faf405f2e
tumelorabale <133318673+tumelorabale@users.noreply.github.com>
2023-07-21 21:40:53 -0500

    title&year-added

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import '../styles/MovieList.css'
 
 const MovieList = (props) => {
 	return (
 		<>
 			{props.movies.map((movie, index) => (
-				<div className='image-container d-flex justify-content-start m-3'>
+				<div className='image-container'>
 					<img src={movie.Poster} alt='movie'></img>
+					<p className='title'><b></b>{movie.Title}<b/></p>
+					<p className='year'>{movie.Year}</p>
 				</div>
 			))}
 		</>
 	);
 };
-
 export default MovieList;
